480f2b58f15cbff6066f9db3b47262ad0ed62f5f
ariel <ariel@Angelo>
2016-08-24 10:33:41 -0500

    Hice una suma :D!

--- DeberIngenieria/src/deberingenieria/DeberIngenieria.java
+++ DeberIngenieria/src/deberingenieria/DeberIngenieria.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package deberingenieria;
+import java.util.Scanner;
 
 /**
  *
@@ -18,6 +20,25 @@ public class DeberIngenieria {
         // TODO code application logic here
         
         System.out.println("Hola Profe :* ");
-    }
-    
+        
+        Scanner s = new Scanner(System.in);
+int numero1 = 0;
+int numero2 = 0;
+int resultado;
+
+System.out.println("Introduce el primer número:");
+numero1 = s.nextInt();
+
+System.out.println("Introduce el segundo número:");
+numero2 = s.nextInt();
+
+resultado = numero1+numero2;
+
+System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado ) ;
+
 }
+
+}
+
+    
+
